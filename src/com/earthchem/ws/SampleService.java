274b7b88a4dc1b87284313120b91101c2fe8aa3a
bhchen8 <bhchen@ldeo.columbia.edu>
2017-01-09 16:58:27 -0500

    updated

--- src/com/earthchem/ws/SampleService.java
+++ src/com/earthchem/ws/SampleService.java
@@ -6,40 +6,52 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.earthchem.dao.CitationDao;
+import com.earthchem.dao.EarthChemDataDao;
+import com.earthchem.dao.EarthChemModelDao;
+import com.earthchem.dao.EarthChemSampleDao;
+import com.earthchem.dao.GeographyDao;
 import com.earthchem.dao.MaterialDao;
+import com.earthchem.dao.PhaseDao;
+import com.earthchem.dao.SampletypeDao;
+import com.earthchem.model.Citation;
+import com.earthchem.model.EarthChemData;
+import com.earthchem.model.EarthChemModel;
+import com.earthchem.model.EarthChemSample;
+import com.earthchem.model.Geography;
 import com.earthchem.model.Location;
 import com.earthchem.model.Material;
-import com.earthchem.model.Method;
+import com.earthchem.model.Phase;
 import com.earthchem.model.PointType;
+import com.earthchem.model.Sampletype;
+
+/**
+* Receive request and send xml by web service
+*
+* @author  Bai
+* @version 1.0
+* @since   2017-01-04 
+*/
 
 @Path("/SampleService")
 public class SampleService {
-/*	
-	
-	*/
+
 	
 	@GET
 	@Path("/samples")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayPlainTextHello() {
-		return "Hello Jersey";
+		return "Hello Jersey2";
 	}
 	
-	@GET
-	@Path("/locations")
-	@Produces(MediaType.APPLICATION_XML)
-	public Location getLocation() {
-	//	Location location = new Location(null, new LineStringType("fff"),("345"));
-		Location location = new Location(new PointType("fff"),null,null);
-		return  location;
-	}
-	
-	
+
+
 	@GET
 	@Path("/samples/{sampleNum}")
 	@Produces(MediaType.APPLICATION_XML)
-	public Material getSample(@PathParam("sampleNum") int sampleNum){
-		return new MaterialDao().getMaterial(26197, "1282");
-	}
-		
+	public EarthChemModel getEarthChemModel(@PathParam("sampleNum") int sampleNum){
+		System.out.println("bc-EarthChemSample");
+		return new EarthChemModelDao().getEarthChemModel(sampleNum);
+	}	
 }
